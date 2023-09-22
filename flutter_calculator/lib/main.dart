40e4cc7b7486ecaedc19b5f31687f42655111273
Imron jehleh <imron.jehleh@gmail.com>
2023-09-23 04:27:43 +0700

    Added Button
    ADDED:
        lib/main.dart
            Add button, using elevatedButton(), object.

--- flutter_calculator/lib/main.dart
+++ flutter_calculator/lib/main.dart
@@ -39,6 +39,12 @@ class MyHomePage extends StatelessWidget {
         children: [
           Text('A random AWESOME idea:'),
           Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+            },
+            child: Text('Next'),
+          ),
         ],
       ),
     );
