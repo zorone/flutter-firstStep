6bf5c947198d9a6d8b007b7ca4b3d4948907f23c
Imron jehleh <imron.jehleh@gmail.com>
2023-09-25 16:48:17 +0700

    return Card of padding of Text() [Card<-padding<-Text()]
    CHANGED:
        lib/main.dart
            Change behavior of BigCard to return Card widget.
    
    NOTE:
        1. Flutter uses Composition over Inheritance whenever it can. (See OOP concept.)
        2. Perform Ctrl+'.' on Padding()->"Wrap with widget..."->change "widget" to "Card".

--- flutter_calculator/lib/main.dart
+++ flutter_calculator/lib/main.dart
@@ -67,9 +67,11 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Text(pair.asLowerCase),
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asLowerCase),
+      ),
     );
   }
 }
