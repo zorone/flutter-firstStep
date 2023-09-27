b81da377e4901841c23088f5d818e21bb164037a
Imron jehleh <imron.jehleh@gmail.com>
2023-09-27 16:07:24 +0700

    Change in font size and color
    CHANGED:
        lib/main.dart
            declare style for using with text.
    
    NOTE:
        wordList:
            theme.textTheme:
                bodyMedium, caption, headlineLarge, displayMedium
    
            '!' operator (Bang operator):
                flutter is null-safe language, by putting '!' after method, it will tell flutter that you know what you are doing. And you will handle for 'NULL' operation.
    
            copyWith():
                return copied theme which has been altered by the changes you defined.
    
            onPrimary != primary:
                onPrimary: color that is best suited to be written on primary color.
                primary: primary color of specified theme.
    
            ... padding: const EdgeInsets.all(px),
    
        CODE:
            1) final style = theme.textTheme.displayMedium!.copyWith(
                 color: theme.colorScheme.onPrimary,
               );
    
            2) ... child: Padding(
                     padding: ...,
                     child: Text(pair.asLowerCase, style: style),
                   ), ...
    
        MORE:
            1) to show other copyWith() argument, move editing cursor inside parentheses of copyWith(), then Ctrl+Shift+Space to show details.
            2) Above method could works for Card() too. Example, if you want to inrease card's shadow, try adjust 'elevation' parameter's value.
            3) For color, there is more options to choose than theme.colorScheme.primary;
                .primary .secondary .surface
                .onPrimary .onSecondary .onSurface

--- flutter_firstStep/lib/main.dart
+++ flutter_firstStep/lib/main.dart
@@ -68,12 +68,15 @@ class BigCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
 
     return Card(
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase),
+        child: Text(pair.asLowerCase, style: style),
       ),
     );
   }
