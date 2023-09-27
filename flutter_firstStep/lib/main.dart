6a85bed105e3ded820fce32c60e8962c63e49968
Imron jehleh <imron.jehleh@gmail.com>
2023-09-27 16:50:16 +0700

    Center myHomePage
    CHANGED:
        lib/main.dart
            set Column() alignment to center.
    
    NOTE:
        wordList:
            Column():
                mainAxisAlignment: MainAxisAlignment.center,
    
        more:
            By default, Column() will align children from top to bottom.

--- flutter_firstStep/lib/main.dart
+++ flutter_firstStep/lib/main.dart
@@ -42,6 +42,7 @@ class MyHomePage extends StatelessWidget {
 
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text('A random AWESOME idea:'),
           BigCard(pair: pair),
