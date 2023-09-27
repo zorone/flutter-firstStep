a280e76a3322a3d5d0c5e21c15f2a5caee4e5a9f
Imron jehleh <imron.jehleh@gmail.com>
2023-09-27 16:41:10 +0700

    Implement Accessibility feature for Random Text.
    ADDED:
        lib/main.dart
            Added semanticsLabel for random Words; seperate 2 word out to prevent program from recognised some letter as the same word.
    
    NOTE:
        wordList:
            flutter: accessibility Implement by default :core
            operation: String Interpolation; "${pair.first} ${pair.second}" :concept
            Text(): 'SemanticsLabel' attribute :method
    
        List:
            Text()->attr: semanticsLabel: "str"

--- flutter_firstStep/lib/main.dart
+++ flutter_firstStep/lib/main.dart
@@ -76,8 +76,11 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase, style: style),
-        semanticsLabel: "${pair.first} ${pair.second}",
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
