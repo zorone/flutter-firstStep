e80dde0da02944ac78a518fed374aa7d2a497700
Imron jehleh <imron.jehleh@gmail.com>
2023-10-10 14:35:17 +0700

    Experiment with 2 expanded.
    CHANGED:
        lib/main.dart
            in MyHomePage Class
                Wrap SafeArea() with Expanded.
    
    NOTE:
        Tips: Use Ctrl+. to toggle options.

--- flutter_firstStep/lib/main.dart
+++ flutter_firstStep/lib/main.dart
@@ -51,23 +51,25 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Row(
         children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home),
-                  label: Text('Home'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite),
-                  label: Text('Favorites'),
-                ),
-              ],
-              selectedIndex: 0,
-              onDestinationSelected: (value) {
-                print('selected: $value');
-              },
+          Expanded(
+            child: SafeArea(
+              child: NavigationRail(
+                extended: false,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: 0,
+                onDestinationSelected: (value) {
+                  print('selected: $value');
+                },
+              ),
             ),
           ),
           Expanded(
